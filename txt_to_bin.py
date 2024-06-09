e5446e802097232b416bafee46f344f4a2e95ec9
Paper ID: 4402 (LLsM), ACMMM2024 <164483164+Anonymous-conferences-submission@users.noreply.github.com>
2024-06-09 16:10:14 +0800

    Delete txt_to_bin.py

--- txt_to_bin.py
+++
@@ -1,9 +0,0 @@
-def text_to_binary(text):
-    byte_array = text.encode()
-    binary_array = [format(byte, '08b') for byte in byte_array]
-    binary_string = ''.join(binary_array)
-    return binary_string
-
-text = "Multimedia"
-binary_string = text_to_binary(text)
-print(binary_string) 
